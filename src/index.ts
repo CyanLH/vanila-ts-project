b15c069f5c82810e3bfe68c3451516a058ecee59
CyanLH <dlgksk5@gmail.com>
2021-10-23 02:53:32 +0900

    1. window.location.replace() 로 snake.html 이동 추가 2. *.js 제거

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,7 @@ const init = () => {
   const div = document.getElementById('d');
   if (div) {
     div.addEventListener('click', (evt) => {
-      console.log('test', evt);
-      // window.location.hash.replace()
+      window.location.replace('pages/snake.html');
     });
   }
 };
